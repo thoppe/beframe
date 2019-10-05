96354edcf0a4cd7fd6548f293794bfae8f109620
Travis Hoppe <travis.hoppe@gmail.com>
2019-10-05 16:08:45 -0400

    Add all cmd

--- beframe.py
+++ beframe.py
@@ -3,6 +3,7 @@
 Usage:
   beframe.py shot <name>
   beframe.py join <name>
+  beframe.py all <name>
 
 Options:
   --<name>      Movie filename
@@ -11,30 +12,37 @@ Options:
 from docopt import docopt
 import os
 
-if __name__ == '__main__':
-    args = docopt(__doc__, version='Beframe 1.0')
+pipeline_cmds = {
+    "shot": [
+        "P1_shot_detection.py",
+        "P2_scene_change.py",
+        "P3_measure_scenes.py",
+    ],
+    "join": [
+        "E0_extract_segments.py",
+        "E1_extract_audio.py",
+        "E2_combine.py",
+        "E3_detect_speech.py",
+        "E4_build_subset.py",
+    ],
+}
 
-    if args['shot']:
-        pipeline = [
-            'P1_shot_detection.py',
-            'P2_scene_change.py',
-            'P3_measure_scenes.py',
-        ]
+pipeline_cmds["all"] = pipeline_cmds["shot"] + pipeline_cmds["join"]
 
-    elif args['join']:
-        pipeline = [
-            'E0_extract_segments.py',
-            'E1_extract_audio.py',
-            'E2_combine.py',
-            'E3_detect_speech.py',
-            'E4_build_subset.py',         
-        ]
-        pass
 
-    f_movie = args['<name>']
+if __name__ == "__main__":
+    args = docopt(__doc__, version="Beframe 1.0")
+
+    if args["shot"]:
+        pipeline = pipeline_cmds["shot"]
+    elif args["join"]:
+        pipeline = pipeline_cmds["join"]
+    elif args["all"]:
+        pipeline = pipeline_cmds["all"]
+
+    f_movie = args["<name>"]
 
     for call in pipeline:
-        cmd = f'python {call} {f_movie}'
+        cmd = f"python {call} {f_movie}"
         print(cmd)
         os.system(cmd)
-    
